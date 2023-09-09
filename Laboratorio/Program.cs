5c359877a2aa7f953aaa716b984328d0dd2ab5e3
Kenny Sical <kusical@correo.url.edu.gt>
2023-09-08 23:41:03 -0600

    Creacion de clases persona y instruccion

--- Laboratorio/Program.cs
+++ Laboratorio/Program.cs
@@ -13,3 +13,25 @@ namespace Laboratorio
         }
     }
 }
+
+
+#region Clases Persona
+public class Person
+{
+    public string Name { get; set; }
+    public string DPI { get; set; }
+    public string DateBirth { get; set; }
+    public string Address { get; set; }
+
+    public int CompareTo(Person other)
+    {
+        return DPI.CompareTo(other.DPI);
+    }
+
+}
+public class CSVDatos
+{
+    public string Instruction { get; set; }
+    public Person PersonData { get; set; }
+}
+#endregion
